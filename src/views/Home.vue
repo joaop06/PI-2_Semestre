0c45874e0ff75fc0ffc3be737cb8d0814120ffca
joaop06 <joaoofficialpedro@gmail.com>
2023-03-31 13:10:19 -0300

    Page Início

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <Navbar/>
+<v-app>
+  <v-layout>
+    <v-main>
+      <v-container class="d-flex">
+        <v-card width="50vw" height="65vh" :elevation="10">
+          <v-app-bar-title color="red" text="Pedidos em Andamento"></v-app-bar-title>
+        </v-card>
 
-  <v-card width="15vw" height="55vh" :elevation="10">
-  
-    <v-app-bar-title color="blue">Pedidos em Andamento</v-app-bar-title>
 
-
-  
-  </v-card>
-
-
+        <v-row>
+          <v-col v-for="n in 14" :key="n" cols="4" max-width="50vw">
+            <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
+            <v-card :elevation="4" :title="`Produto ${n}`" :subtitle="`O Produto ${n} contém...`"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-layout>
+</v-app>
 </template>
 
 <script>
