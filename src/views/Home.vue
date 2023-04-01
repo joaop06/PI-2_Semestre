f6d6c98cc78753a1d229ce00b088e86f5d3f1c16
joaop06 <joaoofficialpedro@gmail.com>
2023-03-31 21:10:46 -0300

    Att Page Início

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,20 @@
   <Navbar/>
 <v-app>
   <v-layout>
-    <v-main>
-      <v-container class="d-flex">
-        <v-card width="50vw" height="65vh" :elevation="10">
+      <v-main class="d-flex pa-10">
+        <v-card width="80vw" height="65vh" class="mr-10 pa-12" :elevation="8" rounded="lg">
           <v-app-bar-title color="red" text="Pedidos em Andamento"></v-app-bar-title>
         </v-card>
 
+        <v-row class="pa-5">
+          <v-col v-for="n in 28" :key="n" cols="3">
 
-        <v-row>
-          <v-col v-for="n in 14" :key="n" cols="4" max-width="50vw">
             <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
-            <v-card :elevation="4" :title="`Produto ${n}`" :subtitle="`O Produto ${n} contém...`"></v-card>
+            <v-card :elevation="4" :title="`Produto ${n}`" :subtitle="`O Produto ${n} contém...`" text="Props give you an easy interface to display text-only content. They can also be used to easily render images and icons."></v-card>
+
           </v-col>
         </v-row>
-      </v-container>
-    </v-main>
+      </v-main>
   </v-layout>
 </v-app>
 </template>
