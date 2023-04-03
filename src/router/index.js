b6de6390401d653ac63151d34d8f6bc1c062bdf4
Bortoliero <gabortoliero@gmail.com>
2023-04-03 20:53:20 -0300

    tela login admin

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
     name: 'Perfil',
     component: () => import('@/views/Perfil.vue'),
   },
+  
+  {
+    path: '/admin',
+    name: 'login',
+    component: () => import('@/views/admin/login.vue'),
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/admin/login.vue
@@ -0,0 +1,10 @@
+<template>
+    
+    <v-card class="bg-red ma-auto pa-7" title="Login Admin" height="55vh" width="25vw" :elevation="7" variant="outlined">
+        <v-text-field label="Login"></v-text-field>
+        <v-text-field label="Senha"></v-text-field>
+    <v-btn class="bg-black ma-4 pa-7">Click me</v-btn>
+
+    </v-card>
+    
+</template>
