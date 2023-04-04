6e6abdffed27274e7cecc73d1979c388362eb793
Bortoliero <gabortoliero@gmail.com>
2023-04-03 22:26:01 -0300

    style admin

--- src/views/admin/login.vue
+++ src/views/admin/login.vue
@@ -1,8 +1,9 @@
 <template>
+    <navbar v-if="false"></navbar>
     
-    <v-card class="bg-red ma-auto pa-7" title="Login Admin" height="55vh" width="25vw" :elevation="7" variant="outlined">
+    <v-card class="bg-blue-accent-2 ma-auto pa-7" title="Login Admin" height="55vh" width="25vw" :elevation="7" variant="outlined">
         <v-text-field label="Login"></v-text-field>
-        <v-text-field label="Senha"></v-text-field>
+        <v-text-field label="Senha" type="password"></v-text-field>
     <v-btn class="bg-black ma-4 pa-7">Click me</v-btn>
 
     </v-card>
