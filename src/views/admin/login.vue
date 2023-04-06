dc4c71a569baf326fd51f2711ff12857919fbc81
Bortoliero <gabortoliero@gmail.com>
2023-04-05 21:14:47 -0300

    sincronização

--- src/views/admin/login.vue
+++ src/views/admin/login.vue
@@ -1,6 +1,6 @@
 <template>
     
-    <v-card class="bg-red-accent-2 ma-auto pa-7" title="Login Admin" height="55vh" width="25vw" :elevation="7" variant="outlined">
+    <v-card class="bg-red-accent-2 ma-auto pa-7" title="Login Admin" height="500px" width="50%" :elevation="7" variant="outlined">
         <v-text-field label="Login"></v-text-field>
         <v-text-field label="Senha" type="password"></v-text-field>
     <v-btn class="bg-black ma-4 ">Acessar</v-btn>
