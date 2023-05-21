48c601d189956b5f184a5a6e320f241dd4789dd6
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-05-21 18:05:58 -0300

    Script de Produtos BD

--- DataBase/CREATE DATABASE AND TABLES.sql
+++ DataBase/CREATE DATABASE AND TABLES.sql
@@ -6,10 +6,29 @@ CREATE TABLE Produtos (
     nome VARCHAR(50) NOT NULL,
     descricao TEXT NOT NULL,
     preco DECIMAL(5,2) NOT NULL,
-    tipo VARCHAR(1),
+    tipo VARCHAR(15),
     imagem TEXT
 );
 SELECT * FROM Produtos;
+DROP TABLE Produtos;
+
+INSERT INTO produtos (nome, descricao, preco, tipo, imagem) VALUES
+    ('Hambúrguer', 'Delicioso hambúrguer caseiro', 10.50, 'Lanche', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Hamburguer.jpg?raw=true'),
+    ('Pizza Margherita', 'Pizza tradicional com molho de tomate, queijo e manjericão', 25.00, 'Pizza', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Pizza.jpg?raw=true'),
+    ('Batata Frita', 'Porção de batata frita crocante', 8.50, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('Bolinho de Queijo', 'Deliciosos bolinhos de queijo', 7.00, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('Sanduíche de Frango', 'Sanduíche com frango grelhado e vegetais frescos', 12.50, 'Lanche', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Hamburguer.jpg?raw=true'),
+    ('Pizza Calabresa', 'Pizza com calabresa, queijo e molho de tomate', 27.00, 'Pizza', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Pizza.jpg?raw=true'),
+    ('Porção de Onion Rings', 'Cebolas empanadas e fritas', 9.00, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('Bolinho de Bacalhau', 'Bolinhos de bacalhau bem temperados', 8.50, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('X-Burger', 'Hambúrguer, queijo, alface, tomate e maionese', 11.00, 'Lanche', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Hamburguer.jpg?raw=true'),
+    ('Pizza Quatro Queijos', 'Pizza com quatro tipos de queijo derretidos', 28.50, 'Pizza', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Pizza.jpg?raw=true'),
+    ('Batata Rústica', 'Batatas rústicas assadas com ervas', 9.50, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('Bolinho de Carne', 'Bolinhos de carne saborosos', 7.50, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true'),
+    ('X-Salada', 'Hambúrguer, queijo, alface, tomate, cebola e maionese', 11.50, 'Lanche', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Hamburguer.jpg?raw=true'),
+    ('Pizza Portuguesa', 'Pizza com presunto, queijo, cebola, ovo e azeitonas', 29.00, 'Pizza', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Pizza.jpg?raw=true'),
+    ('Nuggets de Frango', 'Porção de nuggets de frango crocantes', 8.00, 'Porção', 'https://github.com/joaop06/imagens-PI-2_Semestre/blob/main/Por%C3%A7%C3%B5es.png?raw=true');
+
 
 
 CREATE TABLE Clientes (
