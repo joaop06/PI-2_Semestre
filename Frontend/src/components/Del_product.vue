22d334f04ad87717cfa2a602b9875820071f8687
Bortoliero <gabortoliero@gmail.com>
2023-06-04 15:20:30 -0300

    fuction delete itens-admin

--- Frontend/src/components/Del_product.vue
+++ Frontend/src/components/Del_product.vue
@@ -3,13 +3,13 @@
         <v-card rounded="xl" class="pa-8">
             <v-row no-gutters justify="space-between">
                 <h2 class="pl-3 pr-3 pa-2">Excluir Item</h2>
-                <v-btn icon="mdi-close" variant="icon"  @click="close">
+                <v-btn icon="mdi-close" variant="icon" @click="close">
                 </v-btn>
             </v-row>
-            
+
             <h3 class="pl-3 pr-3 ma-2">Confirme a exclusão do Item {{ produto.nome }}!</h3>
             <div class="pl-3 pr-3 pb-4">
-                <v-btn class="popup-close bg-orange ma-1" @click="delete">
+                <v-btn class="popup-close bg-orange ma-1" @click="deleteProduct()">
                     Excluir
                 </v-btn>
             </div>
@@ -22,31 +22,39 @@
 import apiURL from '@/services/apiURL'
 
 export default {
-    props:{
+    props: {
 
     },
-    data(){
-        return{
+    data() {
+        return {
             dialog: false,
             produto: {
-            id: '',
-            nome: ''
+                id: '',
+                nome: ''
             }
-        } 
+        }
     },
     methods: {
-        delete(){
+        deleteProduct() {
             const body = {
-                id: id
-            }
-            apiURL.delete('/delete-product', body).then(response => {
-                console.log(response)})
-            this.dialog = false
+                id: this.produto.id
+            };
 
-        },
-        close(){
+            apiURL.delete('/delete-product', { data: body })
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            this.dialog = false;
+            console.log(body.id);
+        }
+        ,
+        close() {
             this.dialog = false
-        }   
+        }
     }
 }
 </script>
