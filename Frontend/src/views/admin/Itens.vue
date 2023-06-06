9432b3fa395a8da5e42dab2ce74920bbaf463d57
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-06-05 21:23:22 -0300

    Tela Pedidos Gerais

--- Frontend/src/views/admin/Itens.vue
+++ Frontend/src/views/admin/Itens.vue
@@ -1,86 +1,78 @@
 <template>
   <main>
-    <v-card>
-      <v-layout>
-        <v-navigation-drawer rail permanent style="width: 50px;" class="bg-red-accent-2 elevation-7">
+    <v-row>
+      <NavbarAdmin />
+      <v-col class="pa-1">
 
-            <v-btn to="admin/PedidosGerais" icon="mdi-circle" class="elevation-1" variant="tonal"></v-btn>
 
-            <v-btn to="/admin" variant="tonal" icon="mdi-logout" class="elevation-1"></v-btn>
+        <div class="bg-blue-lighten-3">
+          <v-row class="d-flex pa-7" justify="end">
+            <v-btn @click="eventActiveNewProducts" class="bg-orange" :style="{ 'justify-content': 'end' }"
+              prepend-icon="mdi-plus">
+              Cadastrar Novo Produto
+            </v-btn>
+          </v-row>
 
-        </v-navigation-drawer>
+          <v-col cols="12" rounded="xl">
+            <v-row justify="start">
 
-        <v-main>
-          <div>
-            <v-row class="d-flex pa-7" justify="space-between">
-              <v-btn @click="eventActiveNewProducts" class="bg-orange"  :style="{ 'justify-content': 'end' }" prepend-icon="mdi-plus">
-                Cadastrar Novo Produto
-              </v-btn>
+              <v-col v-for="(produto, index) in cardapio" class="d-flex justify-space-around aligin-center" :key="index"
+                cols="4">
+
+                <v-card color="rgb(0, 0, 0, 0)" rounded="lg" width="25vw" height="30vh" min-height="200px">
+
+                  <v-img class="image justify-space-between align-end d-flex"
+                    gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,0.6), rgba(0,0,0,1)" :src="produto.imagem"
+                    :lazy-src="produto.imagem" cover min-height="200px">
+                    <template v-slot:placeholder>
+                      <div class="d-flex align-center justify-center fill-height">
+                        <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
+                      </div>
+                    </template>
+
+                    <v-btn @click="eventActiveDelProducts(produto)" class="ma-4 bg-red-accent-2" rounded="lg">
+                      <v-icon>
+                        mdi-delete
+                      </v-icon>
+                    </v-btn>
+
+                    <v-card color="transparent" :elevation="0">
+
+                      <v-card-title>
+                        <span class="text-h6 text-white text-bold">{{ produto.nome }}</span>
+                      </v-card-title>
+                      <v-card-subtitle class="mt-n3">
+                        <span class="text-white">{{ produto.descricao }}</span>
+                      </v-card-subtitle>
+
+                      <v-card-text class="my-n4">
+                        <v-row justify="space-between" no-gutters>
+                          <v-card class="font-weight-black d-flex justify-center ma-5 pa-1" color="rgb(255, 255, 255, 0)"
+                            :elevation="0">
+                            <span :style="{ 'color': 'white', 'font-weight': 'bold' }">{{ produto.preco }}</span>
+                          </v-card>
+
+                          <v-btn @click="eventActiveUpdtProducts(produto)" class="ma-4 bg-red-accent-3" rounded="lg">
+                            <v-icon class="mr-2">
+                              mdi-pencil
+                            </v-icon>
+                            Alterar
+                          </v-btn>
+                        </v-row>
+                      </v-card-text>
+                    </v-card>
+                  </v-img>
+                </v-card>
+              </v-col>
             </v-row>
+          </v-col>
+        </div>
 
-            <v-col cols="12" rounded="xl">
-              <v-row justify="start">
-
-                <v-col v-for="(produto, index) in cardapio" class="d-flex justify-space-around aligin-center" :key="index"
-                  cols="4">
-
-                  <v-card color="rgb(0, 0, 0, 0)" rounded="lg" width="25vw" height="30vh" min-height="200px">
-
-                    <v-img class="image justify-space-between align-end d-flex"
-                      gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,0.6), rgba(0,0,0,1)" :src="produto.imagem"
-                      :lazy-src="produto.imagem" cover min-height="200px">
-                      <template v-slot:placeholder>
-                        <div class="d-flex align-center justify-center fill-height">
-                          <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
-                        </div>
-                      </template>
-
-                      <v-btn @click="eventActiveDelProducts(produto)" class="ma-4 bg-red-accent-2" rounded="lg">
-                        <v-icon>
-                          mdi-delete
-                        </v-icon>
-                      </v-btn>
-
-                      <v-card color="transparent" :elevation="0">
-
-                        <v-card-title>
-                          <span class="text-h6 text-white text-bold">{{ produto.nome }}</span>
-                        </v-card-title>
-                        <v-card-subtitle class="mt-n3">
-                          <span class="text-white">{{ produto.descricao }}</span>
-                        </v-card-subtitle>
-
-                        <v-card-text class="my-n4">
-                          <v-row justify="space-between" no-gutters>
-                            <v-card class="font-weight-black d-flex justify-center ma-5 pa-1"
-                              color="rgb(255, 255, 255, 0)" :elevation="0">
-                              <span :style="{ 'color': 'white', 'font-weight': 'bold' }">{{ produto.preco }}</span>
-                            </v-card>
-
-                            <v-btn @click="eventActiveUpdtProducts(produto)" class="ma-4 bg-red-accent-3" rounded="lg">
-                              <v-icon class="mr-2">
-                                mdi-pencil
-                              </v-icon>
-                              Alterar
-                            </v-btn>
-                          </v-row>
-                        </v-card-text>
-                      </v-card>
-                    </v-img>
-                  </v-card>
-                </v-col>
-              </v-row>
-            </v-col>
-          </div>
-        </v-main>
-      </v-layout>
-    </v-card>
-
-
-    <NewProducts ref="NewProducts" />
-    <UpdtProducts ref="UpdtProducts" />
-    <DelProducts ref="DelProducts" />
-
+        <NewProducts ref="NewProducts" />
+        <UpdtProducts ref="UpdtProducts" />
+        <DelProducts ref="DelProducts" />
+      </v-col>
+    </v-row>
   </main>
 </template>
 
@@ -90,16 +82,18 @@ import NewProducts from '@/components/New_product.vue';
 import UpdtProducts from '@/components/Updt_product.vue';
 import DelProducts from '@/components/Del_product.vue';
 import apiURL from '@/services/apiURL';
+import NavbarAdmin from '@/components/NavbarAdmin.vue'
 
 export default {
   components: {
     NewProducts,
     UpdtProducts,
-    DelProducts
+    DelProducts,
+    NavbarAdmin
   },
   data() {
     return {
-      cardapio: [] 
+      cardapio: []
 
     }
   },
@@ -115,7 +109,7 @@ export default {
       this.$refs.UpdtProducts.produto.tipo = produto.tipo;
       this.$refs.UpdtProducts.produto.id = produto.id;
     },
-    findItensAdmin(){
+    findItensAdmin() {
       apiURL.get('/admin-itens').then(response => {
         this.cardapio = response.data.data
       })
@@ -126,7 +120,7 @@ export default {
       this.$refs.DelProducts.produto.nome = produto.nome
     }
   },
-  mounted(){
+  mounted() {
     this.findItensAdmin()
   }
 }
--- Frontend/src/views/admin/PedidosGerais.vue
+++ Frontend/src/views/admin/PedidosGerais.vue
@@ -1,7 +1,65 @@
 <template>
-    <h1>PEDIDOS GERAIS</h1>
+    <main>
+        <NavbarAdmin />
+        <v-row>
+            <v-card class="ma-auto mt-12" :elevation="0" color="rgb(0,0,0,0)" width="70%">
 
-    
+                <!--v-for="(pedido, index) in pedidosAndamento" :key="index"-->
+                <v-card class="ma-2 bg-light-green-lighten-4 d-flex flex-column" border="red" rounded="lg"
+                    :style="{ 'border': 'solid 3px #1B5E20' }" :elevation="2">
+
+                    <v-col>
+                        <span class="d-flex justify-space-between">
+                            <p class="text-h5 text-green-dar  ken-4">Pedido: </p>
+
+                            <p class="text-h5 text-green-darken-4">Status: </p>
+                        </span>
+                    </v-col>
+
+                    <v-col class="d-flex justify-space-between my-n5">
+                        <v-col cols="10">
+                            <p class="ma-2">
+                                <span class="text-h6 text-green-darken-4">Produtos:</span>
+                            </p>
+                            <p class="ml-2 text-h6 text-green-darken-4">Total: </p>
+                        </v-col>
+
+                        <v-col>
+                            <v-btn class="ma-auto text-red" icon="mdi-close-thick" color="rgb(0,0,0,0)"
+                                :elevation="0"></v-btn>
+                        </v-col>
+                    </v-col>
+
+                </v-card>
+            </v-card>
+        </v-row>
+    </main>
 </template>
 
-    
+
+<script>
+import NavbarAdmin from '@/components/NavbarAdmin.vue'
+import apiURL from '@/services/apiURL.js'
+
+export default ({
+    components: {
+        NavbarAdmin
+    },
+    data() {
+        return {
+            pedidos_gerais: []
+        }
+    },
+    methods: {
+        async pedidosGerais() {
+            await apiURL.get('/pedidos-gerais').then(response => {
+                if (response.data.data > 0) {
+                    this.pedidos_gerais = response.data.data
+                } else {
+                    console.log("Não há pedidos no momento")
+                }
+            })
+        }
+    }
+})
+</script>
